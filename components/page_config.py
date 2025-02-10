f235a5bbe2538a07a26a4d4f5a7e86690d9ea7b3
Pratik Patil <patil.pratiq@gmail.com>
2025-02-09 19:20:13 -0500

    Updated the chat layout to centered to stop chat overflow

--- components/page_config.py
+++ components/page_config.py
@@ -5,7 +5,7 @@ def set_page_config():
     st.set_page_config(
         page_title="Local RAG",
         page_icon="📚",
-        layout="wide",
+        layout="centered",
         initial_sidebar_state=st.session_state["sidebar_state"],
         menu_items={
             "Get Help": "https://github.com/jonfairbanks/local-rag/discussions",
